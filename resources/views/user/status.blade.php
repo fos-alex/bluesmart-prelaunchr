53c95b14aad2c739ab87fca5e6788bb476698b6b
Alex Otero <fos.alex@gmail.com>
2017-06-11 19:31:34 -0300

    Prelaunch styles

--- resources/views/user/status.blade.php
+++ resources/views/user/status.blade.php
@@ -31,21 +31,25 @@
 								<button id="copy-button" data-clipboard-text="{{ $user->referral_url }}" title="Click to copy" class="btn btn-link">Copy to clipboard</button>
 								<span style="display:none;" id="copy-confirmation"><br/>Copied!</span>
 							</p>
-						</div>	
+						</div>
 					</div>
 
 					<div class="row">
 						<div class="col-xs-12">
 							<div class="social">
 								<div class="fb-share-button" data-href="{{ $user->referral_url }}" data-layout="button"></div>
-
-								<span class="separator">&nbsp;|&nbsp;</span>
-
 								<a href="https://twitter.com/share" class="twitter-share-button" data-url="{{ $user->referral_url }}" data-text="Can't wait for @Bluesmart to launch. My workouts are about to go to the next level!" data-via="Bluesmart" data-size="medium" data-count="none">Tweet</a>
-	<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>	
-							</div>	
-						</div>	
-							
+								<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+							</div>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-lg-12">
+							<div class="scroll-container">
+								<img class="scroll-icon" src="{{ asset('img/flecha.svg') }}"/>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
@@ -53,6 +57,7 @@
 	</header>
 
 	<section>
+
 		<div class="container">
 			<div class="row">
 				<div class="col-xs-12">
@@ -60,45 +65,105 @@
 				</div>
 			</div>
 
-			<div class="row">
-				<div class="col-xs-10 col-xs-offset-1">
-					<div class="progress">
-						<div class="progress-bar" role="progressbar" aria-valuenow="{{ $referral_count + 1 }}" aria-valuemin="0" aria-valuemax="100" style="width:{{ $progress_percent }}%">
-							<span class="sr-only"></span>
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-12">
+						<p class="center referral-count">
+							<span class="number">{{ $referral_count == 0 ? 'No' : 'Friends referred:' . $referral_count }}</span>
+							{{ $referral_count == 1 ? ' friend has' : 'friends have' }} joined{{ $referral_count == 0 ? '...Yet!' : '!' }}
+						</p>
+						{{--					@foreach($referrals as $referral)
+                                            <p class="center text-muted">
+                                                {{ $referral->email }}
+                                            </p>
+                                            @endforeach--}}
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="container">
+		<div class="row referrals">
+			<div class="col-xs-8 col-xs-offset-2">
+				<div class="row referrals-container">
+					<div class="col-xs-2 text-center">
+						<div class="reward-level {{ $referral_count >= 0 ? '' : 'dimmed' }}">
+							<img class="" src="{{ asset('img/0friends.svg') }}" />
+							<div class="blue-dot"></div>
+						</div>
+					</div>
+					<div class="col-xs-2 text-center">
+						<div class="reward-level {{ $referral_count >= $rewards[1]->num_referrals ? '' : 'dimmed' }}">
+							<img class="" src="{{ asset('img/1friend.svg') }}" />
+							<div class="blue-dot"></div>
+						</div>
+					</div>
+					<div class="col-xs-2 text-center">
+						<div class="reward-level {{ $referral_count >= $rewards[2]->num_referrals ? '' : 'dimmed' }}">
+							<img class="" src="{{ asset('img/2friends.svg') }}" />
+							<div class="blue-dot"></div>
+						</div>
+					</div>
+					<div class="col-xs-2 text-center">
+						<div class="reward-level {{ $referral_count >= $rewards[3]->num_referrals ? '' : 'dimmed' }}">
+							<img class="" src="{{ asset('img/3friends.svg') }}" />
+							<div class="blue-dot"></div>
+						</div>
+					</div>
+					<div class="col-xs-2 text-center">
+						<div class="reward-level {{ $referral_count >= $rewards[4]->num_referrals ? '' : 'dimmed' }}">
+							<img class="" src="{{ asset('img/4friends.svg') }}" />
+							<div class="blue-dot"></div>
+						</div>
+					</div>
+					<div class="col-xs-2 text-center">
+						<div class="reward-level {{ $referral_count >= $rewards[5]->num_referrals ? '' : 'dimmed' }}">
+							<img class="" src="{{ asset('img/5friends.svg') }}" />
+							<div class="blue-dot"></div>
 						</div>
 					</div>
 				</div>
 			</div>
+		</div>
 
-			<div class="row referrals">
-				<div class="col-xs-2 col-xs-offset-3">
-					<div class="reward-level">
-						<img src="{{ asset('img/0friends.png') }}" /><span>You</span>
+		<div class="row">
+			<div class="col-xs-10 col-xs-offset-1">
+				<div class="progress">
+					<div class="progress-bar" role="progressbar" aria-valuenow="{{ $referral_count + 1 }}" aria-valuemin="0" aria-valuemax="100" style="width:{{ $progress_percent }}%">
+						<span class="sr-only"></span>
 					</div>
 				</div>
-				<div class="col-xs-2">
-					<div class="reward-level {{ $referral_count >= 1 ? '' : 'dimmed' }}">
-						<img class="" src="{{ asset('img/1friend.png') }}" /><span>1</span>
-					</div>
-				</div>
-				<div class="col-xs-2">
-					<div class="reward-level {{ $referral_count >= 2 ? '' : 'dimmed' }}">
-						<img class="" src="{{ asset('img/2friends.png') }}" /><span>2</span>
-					</div>
-				</div>	
-				<div class="col-xs-2">
-					<div class="reward-level {{ $referral_count >= 3 ? '' : 'dimmed' }}">
-						<img class="" src="{{ asset('img/3friends.png') }}" /><span>3+</span>
-					</div>
-				</div>	
 			</div>
+		</div>
 
-			<div class="row rewards">
-				<div class="col-xs-2 col-xs-offset-1"><span class="axis">Reward</span></div>
-				<div class="col-xs-2 offset-left"><span>{{ $rewards[0]->title }}<span></div>
-				<div class="col-xs-2 offset-left"><span>{{ $rewards[1]->title }}</span></div>
-				<div class="col-xs-2">&nbsp;</div>
-				<div class="col-xs-2 offset-left"><span>{{ $rewards[2]->title }}</span></div>	
+		<div class="row rewards">
+			<div class="col-xs-8 col-xs-offset-2">
+				<div class="row rewards-container">
+					<div class="col-xs-2 text-center">
+							<span>Bluesmart <br><strong>{{ $rewards[0]->title }}</strong></span>
+							<p class="referral-bound"><strong>{{ $rewards[0]->num_referrals }}</strong></strong> friends</p>
+					</div>
+					<div class="col-xs-2 text-center">
+						<span>Bluesmart <br><strong>{{ $rewards[1]->title }}</strong></span>
+						<p class="referral-bound"><strong>{{ $rewards[1]->num_referrals }}</strong></strong> friends</p>
+					</div>
+					<div class="col-xs-2 text-center">
+						<span>Bluesmart <br><strong>{{ $rewards[2]->title }}</strong></span>
+						<p class="referral-bound"><strong>{{ $rewards[2]->num_referrals }}</strong></strong> friends</p>
+					</div>
+					<div class="col-xs-2 text-center">
+						<span>Bluesmart credit <strong>{{ $rewards[3]->title }}</strong></span>
+						<p class="referral-bound"><strong>{{ $rewards[3]->num_referrals }}</strong></strong> friends</p>
+					</div>
+					<div class="col-xs-2 text-center">
+						<span>Bluesmart credit <strong>{{ $rewards[4]->title }}</strong></span>
+						<p class="referral-bound"><strong>{{ $rewards[4]->num_referrals }}</strong></strong> friends</p>
+					</div>
+					<div class="col-xs-2 text-center">
+						<span>Bluesmart credit <strong>{{ $rewards[5]->title }}</strong></span>
+						<p class="referral-bound"><strong>{{ $rewards[5]->num_referrals }}</strong></strong> friends</p>
+					</div>
+				</div>
 			</div>
 		</div>
 	</section>
@@ -107,38 +172,33 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-xs-12">
-					<p class="center referral-count">
-						<span class="number">{{ $referral_count == 0 ? 'No' : $referral_count }}</span>
-						{{ $referral_count == 1 ? ' friend has' : 'friends have' }} joined{{ $referral_count == 0 ? '...Yet!' : '!' }}
-					</p>
 					<p class="center text-muted">
-						@if ($user->valid_email)
-							Keep checking
-						@else
-						You must verify your email to receive rewards.  <a class="resend" href="{{ route('auth.confirm.resend', $user->confirmation_code) }}">Resend confirmation request</a>
+						@if (!$user->valid_email)
+							You must verify your email to receive rewards.  <a class="resend" href="{{ route('auth.confirm.resend', $user->confirmation_code) }}">Resend confirmation request</a>
 						@endif
 					</p>
-				</div>	
-			</div>	
-		</div>	
-	</section>
-
-	@if ($referral_count > 0)
-	<section>
-		<div class="container">
-			<div class="row">
-				<div class="col-xs-12">
-					<p class="center referral-count">
-						Claimed referrals:
-					</p>	
-					@foreach($referrals as $referral)
 					<p class="center text-muted">
-						{{ $referral->email }}	
+						You can come back to this page anytime to track progress.
 					</p>
-					@endforeach
+					<p class="center text-muted no-margin">
+						We will announce the winners once we launch. The countdown begins!
+					</p>
 				</div>
-			</div>	
-		</div>	
+			</div>
+			<div class="row">
+				<div class="col-12 text-center terms-container">
+					<a class="terms" href="#">Terms & Conditions</a>
+				</div>
+			</div>
+		</div>
+		<div class="text-center gray-row">
+			<div class="container">
+				<div class="row">
+					<div class="col-12 isologo-container">
+						<img class="isologo-icon" src="{{ asset('img/isologo.svg') }}"/>
+					</div>
+				</div>
+			</div>
+		</div>
 	</section>
-	@endif
 @stop
