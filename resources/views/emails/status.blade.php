596ec5d4f11f56e6f50e21205af98c92de111a1e
Alex Otero <fos.alex@gmail.com>
2017-06-13 01:37:56 -0300

    Fixes to alert

--- resources/views/emails/status.blade.php
+++ resources/views/emails/status.blade.php
@@ -305,7 +305,6 @@
             <div class="content">
 
                 <!-- START CENTERED WHITE CONTAINER -->
-                <span class="preheader">This is preheader text. Some clients will show this text as a preview.</span>
                 <table class="main">
 
                     <!-- START MAIN CONTENT AREA -->
